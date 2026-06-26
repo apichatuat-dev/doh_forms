6d1c9916f1e31abda388813c542fd13d221e3b1b
not1951aw <not1951aw@gmail.com>
2026-06-26 16:32:19 +0700

    test

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -33,7 +33,7 @@ class HomeController extends Controller
         //     'code' => 'f222c4fe320923d0c1662b597194dfb5a88491574df70c8b0dc52b5718cd0fa8'
         // ]);
         // dd($response);
-        $response = Http::asJson()->post('https://onelogin.doh.go.th/api/authen', [
+        $response = Http::asJson()->post('https://onelogin.doh.go.th:8080/api/authen', [
     'code' => 'f222c4fe320923d0c1662b597194dfb5a88491574df70c8b0dc52b5718cd0fa8',
 ]);
 
