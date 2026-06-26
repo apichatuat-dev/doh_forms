6db680b4ecd4960b80ce25019a7524637810f003
not1951aw <not1951aw@gmail.com>
2026-06-26 16:01:50 +0700

    test one login

--- laravel/resources/views/product/index.blade.php
+++ laravel/resources/views/product/index.blade.php
@@ -1,62 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- @include('product.modal') --}}
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-7">
-            @if (session('message'))
-                <div class="alert alert-success">{{ session('message') }}</div>
-            @endif
-            @if (session('message_error'))
-                <div class="alert alert-danger">{{ session('message_error') }}</div>
-            @endif
-            <h4 class="mb-3">All Products
-                <a class="btn btn-sm btn-outline-success float-end" href="create"><i class="fa fa-plus"></i> Add Product</a>
-            </h4>
-            <div class="card border border-secondary shadow-sm">
-                <div class="card-header bg-dark text-light">{{ __('All Products') }}</div>
+<script>
+    alert("dd")
+    axios.post(
+    'https://onelogin.doh.go.th/api/authen',
+    {
+        code: "f222c4fe320923d0c1662b597194dfb5a88491574df70c8b0dc52b5718cd0fa8"
+    },
+    {
+        withCredentials: true,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+)
+.then(response => {
 
-                <div class="card-body bg-dark">
-                    <table class="table table-dark border border-secondary table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Description</th>
-                                <th>Price</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            
-                            @forelse ($products as $product)
-                            <tr>
-                                <td>{{ $product->id }}</td>
-                                <td>{{ $product->name }}</td>
-                                <td>{{ $product->description }}</td>
-                                <td>${{ $product->price }}</td>
-                                <td class="">
-                                    <a href="{{ url('read/'.$product->id) }}" class="btn btn-sm btn-outline-primary text-decoration-none m-1"><i class="fa fa-eye"></i> </a>
-                                    <a href="{{ url('update/'.$product->id.'/edit') }}" class="btn btn-sm btn-outline-success text-decoration-none m-1"><i class="fa fa-pencil"></i> </a>
-                                        <button class="btn btn-sm btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $product->id }}">
-                                            <span><i class="fa-solid fa-trash"></i></span></span>
-                                        </button>
-                                    </a>
-                                </td>
-                            </tr>
-                            @include('product.modal')
-                            @empty
-                            <tr>
-                                <td class="text-center" colspan="7">No Products Available</td>
-                            </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    console.log(response.data);
 
+    const token = response.data.token;
+
+    // เรียก API user ต่อ
+
+})
+.catch(error => {
+
+    console.error(error.response);
+
+});
+</script>
 @endsection
